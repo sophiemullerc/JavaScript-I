5ccb53c2892222520ff286cba4e61405c37fbc91
sophiemullerc <32464200+sophiemullerc@users.noreply.github.com>
2017-10-08 21:43:33 -0700

    My first commit of my JSI hw
    There still a lot of work to finish.

--- src/closure.js
+++ src/closure.js
@@ -5,17 +5,38 @@ const counter = () => {
   // Example: const newCounter = counter();
   // newCounter(); // 1
   // newCounter(); // 2
+  let newCounter = 0;
+  return () => {
+    return newCounter += 1;
+  };
 };
 
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let newCounter = 0;
+  return {
+    increment: () => {
+      return ++newCounter;
+    },
+    decrement: () => {
+      return --newCounter;
+    },
+  };
 };
 
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let callCount = 0;
+  return () => {
+    if (callCount === n) {
+      return null;
+    }
+    callCount++;
+    return cb();
+  };
 };
 
 /* Extra Credit */
@@ -27,6 +48,14 @@ const cacheFunction = (cb) => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  const cache = {};
+  return (input) => {
+    if (Object.property.hasOwnProperty.call(cache, input)) {
+      return cache[input];
+    }
+    cache[input] = cb(input);
+    return cache[input];
+  };
 };
 
 /* eslint-enable no-unused-vars */
